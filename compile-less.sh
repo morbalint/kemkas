42d3cb5b6578448b9f78bc71848395adef8a2014
Balint Morasz <balint@morasz.hu>
2023-10-26 09:59:45 +0200

    fix deployment

--- compile-less.sh
+++ compile-less.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "Compiling all LESS files to CSS"
-for file in src/**/*.less
-do
-    FROM=$file
-    TO=${file/.*/.css}
-    SOURCE_MAP=${file/.*/.css.map}
-    echo "$FROM --> $TO & $SOURCE_MAP"
-    node_modules/less/bin/lessc "$FROM" "$TO" --source-map="$SOURCE_MAP"
-done
+
+function compile_less() {
+  FROM=$1
+  TO=${FROM/.*/.css}
+  SOURCE_MAP=${FROM/.*/.css.map}
+  echo "$FROM --> $TO & $SOURCE_MAP"
+  node_modules/less/bin/lessc "$FROM" "$TO" --source-map="$SOURCE_MAP"
+}
+
+export -f compile_less
+find src -type f -name '*.less' -exec bash -c 'compile_less $1' shell {} \;
