cd29a14121f75325383f6bd805f432ef15070f2a
morbalint <balint@morasz.hu>
2023-12-20 17:28:57 +0100

    fix ts import syntax

--- Kemkas.Web/frontend/src/first-edition/api/character.api.ts
+++ Kemkas.Web/frontend/src/first-edition/api/character.api.ts
@@ -1,14 +1,14 @@
 import {KarakterInputs} from "../domain-models/karakter";
-import * as axios from "axios";
+import axios from "axios";
 
 export async function StoreNewCharacter(karakter: KarakterInputs) {
-    let response = await axios.default.post(`${window.location.origin}/Character/`, karakter, {
+    let response = await axios.post(`${window.location.origin}/Character/`, karakter, {
         withCredentials: true,
     })
     if (response.status < 300){
         return response.data as string
     }
     else {
-        return Error(response.statusText)
+        throw Error(response.statusText)
     }
 }
