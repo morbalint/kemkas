b132a5801b3d80b37a978147bb4818407ed08c28
morbalint <balint@morasz.hu>
2024-03-09 00:08:09 +0100

    ci: split repo: make this repo frontend only again

--- Kemkas.Web/Db/Models/ApplicationRole.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Kemkas.Web.Db.Models;
-
-public class ApplicationRole : IdentityRole<int>
-{
-    
-}
--- Kemkas.Web/Db/Models/ApplicationUser.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Kemkas.Web.Db.Models;
-
-public class ApplicationUser : IdentityUser<int>
-{
-}
